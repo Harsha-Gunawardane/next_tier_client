837fd3a73ff7e7ec9b0389ab1d8b4a4989b8b543
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-07-10 15:08:38 +0530

    Clean the code little

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import Register from "./features/auth/Register";
 import Login from "./features/auth/Login";
-import Home from "./components/Home";
 import Layout from "./components/Layout";
-import Editor from "./components/Editor";
-import Admin from "./components/Admin";
 import Missing from "./pages/Missing";
 import Unauthorized from "./pages/Unauthorized";
-import Lounge from "./components/Lounge";
-import LinkPage from "./components/LinkPage";
 import RequireAuth from "./features/auth/RequireAuth";
 import PersistLogin from "./features/auth/PersistLogin";
 import UserLayout from "./features/users/UserLayout";
 import UserProfile from "./features/users/components/UserProfile";
+import SidebarAndHeader from './layouts/SidebarAndHeader';
+import Dashboard from './components/Dashboard'
+import Courses from "./components/Courses";
+import Content from "./components/Content";
+import Landing from "./pages/Landing";
+
 import { Routes, Route } from "react-router-dom";
 import { ROLES } from "./config/roles";
 
@@ -19,50 +20,26 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* public routes */}
-        <Route path="/" element={<Landing/>} />
-        <Route path="unauthorized" element={<Unauthorized />} />
-        <Route path="linkpage" element={<LinkPage />} />
 
+        {/* public routes */}
+        <Route index element={<Layout />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
 
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
 
         {/* we want to protect these routes */}
         <Route element={<PersistLogin />}>
-          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-            <Route path="/" element={<Home />} />
-            <Route element={<SidebarAndHeader userRole={'student'} />}>
+
+          <Route element={<RequireAuth allowedRoles={[ROLES.Student]} />}>
+            {/* <Route path="/" element={<Home />} /> */}
+
+            <Route path="stu" element={<SidebarAndHeader userRole={'student'} />}>
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="courses" element={<Courses />} />
               <Route path="content" element={<Content />} />
             </Route>
           </Route>
-
-          <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
-            <Route path="editor" element={<Editor />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-            <Route path="admin" element={<Admin />} />
-          </Route>
-
-          <Route
-            element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
-          >
-            <Route path="lounge" element={<Lounge />} />
-          </Route>
-
-          {/* <Route
-            element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
-          >
-            <Route path="tutor" element={<TutorLayout />}>
-              <Route path="courses" element={<CourseLayout/>} >
-                <Route index element={<Course />} />
-                <Route path="packages" element={<Package />} />
-              </Route>
-            </Route>
-          </Route> */}
 
           <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
             <Route path="user" element={<UserLayout />}>
--- src/index.js
+++ src/index.js
@@ -5,19 +5,8 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-import React from 'react';
-import { ColorModeScript } from "@chakra-ui/react";
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
-
-
-import { ChakraProvider } from "@chakra-ui/react";
 
 if (process.env.NODE_ENV !== "production") {
   disableReactDevTools();
