d10301a86eac0bc63c422c1976fd32e34ff336b3
Nipuna Rahal <2020cs019@stu.ucsc.cmb.ac.lk>
2023-07-12 09:44:19 +0530

    theme changes

--- src/layouts/SidebarAndHeader.js
+++ src/layouts/SidebarAndHeader.js
@@ -1,64 +1,54 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import Layout from "../components/Layout";  
+import Layout from "../components/Layout";
 import Sidebar from "../components/Sidebar";
-import { SidebarProvider } from '../context/SidebarContext';
+import { SidebarProvider } from "../context/SidebarContext";
 
 import { Container, Flex, Grid } from "@chakra-ui/react";
 
 //icons
-import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
-import {TiDocumentText} from 'react-icons/ti';
-import {FaCompass} from 'react-icons/fa';
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import { TiDocumentText } from "react-icons/ti";
+import { FaCompass } from "react-icons/fa";
 
-const SidebarAndHeader = ({userRole}) => {
-    var Options = []
-    const StuOptions = [
-        { icon: GridViewRoundedIcon, name: 'Dashboard',value: 'dashboard' , href: '/dashboard'},
-        { icon: TiDocumentText, name: 'Courses',value: 'courses', href: '/courses'},
-        { icon: FaCompass, name: 'Content',value: 'content', href: '/content'},
-    ]
+const SidebarAndHeader = ({ userRole }) => {
+	var Options = [];
+	const StuOptions = [
+		{ icon: GridViewRoundedIcon, name: "Dashboard", value: "dashboard", href: "/dashboard" },
+		{ icon: TiDocumentText, name: "Courses", value: "courses", href: "/courses" },
+		{ icon: FaCompass, name: "Content", value: "content", href: "/content" },
+	];
 
-    const TeacherOptions = [
-        { icon: GridViewRoundedIcon, name: 'Dashboard',value: 'dashboard' , href: '/dashboard'},
-        { icon: TiDocumentText, name: 'Courses',value: 'courses', href: '/courses'},
-    ]
+	const TeacherOptions = [
+		{ icon: GridViewRoundedIcon, name: "Dashboard", value: "dashboard", href: "/dashboard" },
+		{ icon: TiDocumentText, name: "Courses", value: "courses", href: "/courses" },
+	];
 
-    switch(userRole){
-        case('student'):
-            Options = StuOptions
-            break;
-        case('teacher'):
-            Options = TeacherOptions
-            break;
-        default:
-            Options = StuOptions
-    }
+	switch (userRole) {
+		case "student":
+			Options = StuOptions;
+			break;
+		case "teacher":
+			Options = TeacherOptions;
+			break;
+		default:
+			Options = StuOptions;
+	}
 
-    //chakra ui layout for sidebar from components folder and header with an outlet for the children
-    return (
-
-        <Grid 
-            w="100vw"
-            templateAreas={`"sidebar header" "sidebar main"`}
-            templateColumns={{ base: 'auto 1fr', md: 'auto 1fr' }}
-            templateRows={{ base: 'auto 1fr', md: 'auto 1fr' }}
-            scrollBehavior={'smooth'}
-        >
-            <SidebarProvider>
-                <Sidebar Options={Options} minimized={false}/>
-            </SidebarProvider>
-            <Flex direction={'column'} w={'full'} h={'100vh'} overflow={'hidden'}>
-                <Header/>
-                <div>
-                    <Outlet/>
-                </div>
-                
-            </Flex>
-        </Grid>
-    )
-
-}
-
+	//chakra ui layout for sidebar from components folder and header with an outlet for the children
+	return (
+		<Grid w="100vw" templateAreas={`"sidebar header" "sidebar main"`} templateColumns={{ base: "auto 1fr", md: "auto 1fr" }} templateRows={{ base: "auto 1fr", md: "auto 1fr" }} scrollBehavior={"smooth"} overflow={"scroll"}>
+			<SidebarProvider>
+				<Sidebar Options={Options} minimized={false} />
+			</SidebarProvider>
+			<Flex direction={"column"} w={"full"} h={"100vh"} overflow={"hidden"}>
+				<Header />
+				<div>
+					<Outlet />
+				</div>
+			</Flex>
+		</Grid>
+	);
+};
 
 export default SidebarAndHeader;
