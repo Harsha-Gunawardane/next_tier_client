711dcfa049d1623cfbc1e82a2c40f19bad857dad
Nipuna Rahal <2020cs019@stu.ucsc.cmb.ac.lk>
2023-07-15 12:31:47 +0530

    fixed

--- src/layouts/SidebarAndHeader.js
+++ src/layouts/SidebarAndHeader.js
@@ -1,104 +1,81 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
 import Sidebar from "../components/Sidebar";
-import { SidebarProvider } from "../context/SidebarContext";
+import {SidebarProvider, SidebarContext} from "../context/SidebarContext";
+import {useContext, useEffect, useState} from "react";
 
-import { Box, Container, Flex, Grid, GridItem } from "@chakra-ui/react";
+import {Box, Container, Flex, Grid, GridItem} from "@chakra-ui/react";
 
 //icons
 import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
-import { TiDocumentText } from "react-icons/ti";
-import { FaCompass, FaList, FaQuestionCircle, FaUserFriends } from "react-icons/fa";
+import {TiDocumentText} from "react-icons/ti";
+import {FaCompass} from "react-icons/fa";
+import {TbChevronsUpLeft} from "react-icons/tb";
 
-const SidebarAndHeader = ({ userRole }) => {
-  var Options = [];
-  const StuOptions = [
-    {
-      icon: GridViewRoundedIcon,
-      name: "Dashboard",
-      value: "dashboard",
-      href: "/dashboard",
-    },
-    {
-      icon: TiDocumentText,
-      name: "Courses",
-      value: "courses",
-      href: "/courses",
-    },
-    { icon: FaCompass, name: "Content", value: "content", href: "/content" },
-  ];
+const SidebarAndHeader = ({userRole}) => {
+	//get width of sidebar component and set to state
+	const [sidebarWidth, setSidebarWidth] = useState("");
 
-  const TeacherOptions = [
-    {
-      icon: GridViewRoundedIcon,
-      name: "Dashboard",
-      value: "dashboard",
-      href: "/dashboard",
-    },
-    {
-      icon: TiDocumentText,
-      name: "Courses",
-      value: "courses",
-      href: "/courses",
-    },
-    {
-      icon: FaCompass,
-      name: "Content",
-      value: "content",
-      href: "/content",
-    },
-    {
-      icon: FaUserFriends,
-      name: "Staff",
-      value: "staff",
-      href: "/staff",
-    },
-    {
-      icon: FaList,
-      name: "MCQs",
-      value: "mcq",
-      href: "/mcqpool",
-    },
-    {
-      icon: FaQuestionCircle,
-      name: "Complaints",
-      value: "complaints",
-      href: "/complaints",
-    },
-  ];
+	useEffect(() => {
+		const width = window.getComputedStyle(document.getElementById("Sidebar")).width;
+		setSidebarWidth(width);
+		console.log(sidebarWidth);
+	}, [sidebarWidth]);
 
-  switch (userRole) {
-    case "student":
-      Options = StuOptions;
-      break;
-    case "teacher":
-      Options = TeacherOptions;
-      break;
-    default:
-      Options = StuOptions;
-  }
+	var Options = [];
 
-  //chakra ui layout for sidebar from components folder and header with an outlet for the children
-  return (
-    <Grid
-      w="100vw"
-      templateAreas={`"sidebar header" "sidebar main"`}
-      templateColumns={{ base: "auto 1fr", md: "auto 1fr" }}
-      templateRows={{ base: "auto 1fr", md: "auto 1fr" }}
-      scrollBehavior={"smooth"}
-    >
-      <SidebarProvider>
-        <Sidebar Options={Options} minimized={false} />
-      </SidebarProvider>
-      <Flex direction={"column"} w={"full"} h={"100vh"} overflow={"hidden"}>
-        <Header />
-        <div>
-          <Outlet />
-        </div>
-      </Flex>
-    </Grid>
-  );
+	const StuOptions = [
+		{icon: GridViewRoundedIcon, name: "Dashboard", value: "dashboard", href: "/stu/dashboard"},
+		{icon: TiDocumentText, name: "Courses", value: "courses", href: "/stu/courses"},
+		{icon: FaCompass, name: "Content", value: "content", href: "/stu/content"},
+	];
+
+	const TeacherOptions = [
+		{icon: GridViewRoundedIcon, name: "Dashboard", value: "dashboard", href: "/dashboard"},
+		{icon: TiDocumentText, name: "Courses", value: "courses", href: "/courses"},
+	];
+
+	const StaffOptions = [{icon: GridViewRoundedIcon, name: "Dashboard", value: "dashboard", href: "/dashboard"}];
+
+	switch (userRole) {
+		case "student":
+			Options = StuOptions;
+			break;
+		case "teacher":
+			Options = TeacherOptions;
+			break;
+		default:
+			Options = StuOptions;
+	}
+
+	// console.log(sidebarWidth);
+	//chakra ui layout for sidebar from components folder and header with an outlet for the children
+
+	return (
+		<Box
+			sx={{
+				"&::-webkit-scrollbar": {
+					width: "16px",
+					borderRadius: "8px",
+					backgroundColor: `rgba(0, 0, 0, 0.05)`,
+				},
+				"&::-webkit-scrollbar-thumb": {
+					backgroundColor: `rgba(0, 0, 0, 0.05)`,
+				},
+			}}
+		>
+			<SidebarProvider>
+				<Sidebar Options={Options} minimized={false} setSidebarWidth={setSidebarWidth} />
+			</SidebarProvider>
+			<Box ml={sidebarWidth} w={"calc(100% - " + sidebarWidth + ")"} h={"100vh"}>
+				<Header w={"calc(100% - " + sidebarWidth + ")"} />
+				<Flex as="Middle" pt={"64px"}>
+					<Outlet />
+				</Flex>
+			</Box>
+		</Box>
+	);
 };
 
 export default SidebarAndHeader;
