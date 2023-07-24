7eb0bc9c24a0bebf324b02cf4a13fd2350e84eb8
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-07-24 08:35:58 +0530

    Build done quiz list component

--- src/pages/student/components/quiz/QuizList.js
+++ src/pages/student/components/quiz/QuizList.js
@@ -1,21 +1,36 @@
+import React from "react";
 import { Box } from "@chakra-ui/react";
-
 import DoneQuizCard from "./DoneQuizCard";
 
-function QuizList({ questions, mb }) {
+function QuizList({ mb, questions, userAnswers, onItemClick }) {
+  const getQuestionNo = (trace) => {
+    trace = String(trace + 1);
+    if (trace.length === 1) trace = "0" + trace;
+    return "Question " + trace;
+  };
+
   return (
     <Box>
-      {questions.map((question) => (
-        <DoneQuizCard
-          time={question.time}
-          questionname={question.name}
-          linecolor={question.linecolor}
-          bgcolor={question.bgcolor}
-          mb={mb}
-        />
-      ))}
+      {questions.map((question, index) => {
+        const questionNo = getQuestionNo(index);
+        const linecolor = userAnswers[index] === questions[index].answer ? "#15BD66" : "#EF7373";
+        const bgcolor = userAnswers[index] === questions[index].answer ? "#D3F3D2" : "#FDE6E6";
+
+        return (
+          <DoneQuizCard
+            key={index}
+            i={index}
+            time="40s"
+            questionname={questionNo}
+            linecolor={linecolor}
+            bgcolor={bgcolor}
+            mb={mb}
+            onClick={() => onItemClick(index)} 
+          />
+        );
+      })}
     </Box>
   );
 }
 
-export default QuizList;
+export default React.memo(QuizList);
