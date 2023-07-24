10aa5a1aa820cede1e6a6914d073ea6e8575f0ed
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-07-24 08:37:55 +0530

    Build previous quiz card component

--- src/pages/student/components/cards/PreviousQuizCard.js
+++ src/pages/student/components/cards/PreviousQuizCard.js
@@ -1,22 +1,31 @@
 import { Flex, Box, Text } from "@chakra-ui/react";
 import { AiTwotoneTrophy } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 import ProgressBar from "../quiz/ProgressBar";
 
-function PreviousQuizCard({value, color}) {
+function PreviousQuizCard({ value, color, subject, quizname }) {
+  const navigate = useNavigate()
+
+  const handleOnClick = () => {
+    navigate(`/stu/quizzes/${subject}/${quizname}/review`)
+  }
+
   return (
     <Flex
+      cursor="pointer"
       w={700}
       mb={2}
       h={20}
       borderRadius={8}
       alignItems="center"
       pr={5}
-      border='1px solid #E5E5E5'
+      border="1px solid #E5E5E5"
+      onClick={handleOnClick}
     >
       <Box h="85%" w={1.5} mr={4} ml={2} borderRadius={5} bg="#D5D5D5" />
 
-      <Flex w="100%" justifyContent="space-between" alignItems='center'>
+      <Flex w="100%" justifyContent="space-between" alignItems="center">
         <Box>
           <Text fontSize={17} fontWeight="medium" color="#444444">
             # Quiz 24
