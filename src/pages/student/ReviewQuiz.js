c4e463479f8b99f301a6f127a9c06fa165ad21e3
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-07-24 08:33:29 +0530

    build quiz review page

--- src/pages/student/ReviewQuiz.js
+++ src/pages/student/ReviewQuiz.js
@@ -1,119 +1,66 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import { AiTwotoneTrophy } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
-import ReviewQuizHeader from "./components/quiz/ReviewQuizHeader";
 import McqExplaination from "./components/quiz/McqExplaination";
 import QuizList from "./components/quiz/QuizList";
+import BreadCrumbs from "./components/quiz/BreadCrumbs";
+import { clickOnQuestion, useFetchReviewQuestions } from "../../hooks/fetchReviewQuestions";
+import ResultCard from "./components/cards/ResultCard";
 
 function ReviewQuiz() {
-  const question =
-    "Which of the following statements best describes the Law of Conservation of Energy?";
-  const options = [
-    "Energy can be created but not destroyed.",
-    "Energy can be destroyed but not created.",
-    "Energy cannot be created or destroyed, only transferred or transformed.",
-    "Energy can be transferred but not transformed.",
-  ];
-  const explain = `The Law of Conservation of Energy is a fundamental principle in physics that states that the total amount of energy in an isolated system remains constant over time. Energy cannot be created out of nothing or destroyed completely. It can only change its form or be transferred from one object or system to another. This principle holds true for all types of energy, including kinetic energy, potential energy, thermal energy, electromagnetic energy, and others.
+  const dispatch = useDispatch()
 
-  Option a) "Energy can be created but not destroyed" is incorrect because it implies that energy can be created from nothing, which goes against the Law of Conservation of Energy.
-  
-  Option b) "Energy can be destroyed but not created" is also incorrect because it suggests that energy can be completely eliminated, which contradicts the law.
-  
-  Option d) "Energy can be transferred but not transformed" is incorrect because energy can be both transferred and transformed. For example, when electrical energy is transferred to a light bulb, it is transformed into light energy.
-  
-  Understanding the Law of Conservation of Energy is crucial in physics as it helps explain various phenomena and is applicable across different branches of science and engineering.`;
+  const { subject, mcqname } = useParams();
+  const { isLoading } = useFetchReviewQuestions();
 
-  const questions = [
-    {
-      time: "30s",
-      name: "Question 01",
-      linecolor: "#15BD66",
-      bgcolor: "#D3F3D2",
-    },
-    {
-      time: "30s",
-      name: "Question 02",
-      linecolor: "#15BD66",
-      bgcolor: "#D3F3D2",
-    },
-    {
-      time: "30s",
-      name: "Question 03",
-      linecolor: "#EF7373",
-      bgcolor: "#FDE6E6",
-    },
-    {
-      time: "30s",
-      name: "Question 04",
-      linecolor: "#EF7373",
-      bgcolor: "#FDE6E6",
-    },
-    {
-      time: "30s",
-      name: "Question 05",
-      linecolor: "#15BD66",
-      bgcolor: "#D3F3D2",
-    },
-    {
-      time: "30s",
-      name: "Question 06",
-      linecolor: "#15BD66",
-      bgcolor: "#D3F3D2",
-    },
-    {
-      time: "30s",
-      name: "Question 07",
-      linecolor: "#15BD66",
-      bgcolor: "#FDE6E6",
-    },
-    {
-      time: "30s",
-      name: "Question 08",
-      linecolor: "#15BD66",
-      bgcolor: "#D3F3D2",
-    },
-    {
-      time: "30s",
-      name: "Question 09",
-      linecolor: "#15BD66",
-      bgcolor: "#FDE6E6",
-    },
-    {
-      time: "30s",
-      name: "Question 10",
-      linecolor: "#15BD66",
-      bgcolor: "#D3F3D2",
-    },
-  ];
+  const state = useSelector((state) => state);
+  const trace = useSelector((state) => state.reviewQuiz.trace);
+  const question = useSelector((state) => state.reviewQuiz.queue[state.reviewQuiz.trace]);
+  const pickedAnswer = useSelector((state) => state.reviewQuiz.answers[state.reviewQuiz.trace]);
+
+  const questions = useSelector((state) => state.reviewQuiz.queue);
+  const userAnswers = useSelector((state) => state.reviewQuiz.answers);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (!question) {
+    return <Text>No question found.</Text>;
+  }
+
+  const getQuestionNo = (trace) => {
+    trace = String(trace + 1);
+    if (trace.length === 1) trace = "0" + trace;
+    return "Question " + trace;
+  };
+
+  const handleQuizItemClick = (index) => {
+    console.log("Clicked on Quiz item: ", index);
+    
+    dispatch(clickOnQuestion(index))
+  };
 
   return (
-    <Flex>
+    <Flex pr={10}>
       <Box>
-        <ReviewQuizHeader subject="Physics" quizname="# Quiz 22" />
+        <BreadCrumbs />
         <McqExplaination
-          questionNo="Question 01"
-          question={question}
-          options={options}
-          correctAnswer={2}
-          pickedAnswer={4}
-          explain={explain}
+          questionNo={getQuestionNo(trace)}
+          {...question}
+          pickedAnswer={pickedAnswer}
         />
       </Box>
-      <Box mt={8} mr={16} ml={10}>
-        <Flex
-          w="100%"
-          h={14}
-          justifyContent="space-between"
-          alignItems="center"
-          mb={1}
-        >
-          <Text fontSize={22} color="#444444" fontWeight="medium">
-            # Quiz 22
-          </Text>
-          <AiTwotoneTrophy size={40} color="#15BD66" />
-        </Flex>
-        <QuizList questions={questions} mb={2} />
+      <Box mt={8} ml={10}>
+        <ResultCard />
+        <QuizList mb={2} questions={questions} userAnswers={userAnswers} onItemClick={handleQuizItemClick} />
       </Box>
     </Flex>
   );
