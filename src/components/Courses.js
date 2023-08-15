e11cd07daf8b03871bcd26b0478436b9ce3c1308
Nipuna Rahal <2020cs019@stu.ucsc.cmb.ac.lk>
2023-08-15 16:47:35 +0530

    minor conflict issue fixed

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -3,19 +3,11 @@ import useSidebar from "../hooks/useSidebar";
 
 export default function Courses() {
 
-<<<<<<< HEAD
-    const [minimizeButtonRef] = useOutletContext();
-
-    useEffect(() => {
-        minimizeButtonRef.current.click()
-    }, [])
-=======
     const { setSidebarOptionHandler } = useSidebar();
 
     // useEffect(() => {
     setSidebarOptionHandler("courses")
     // });
->>>>>>> 215282dcc017b8e1c4fe65308641349a7e51efb4
 
     return (
         <div>
