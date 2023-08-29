7096c2b483382aac62cb56eb636bce99bcb12f67
munzira <2020is067@stu.ucsc.cmb.ac.lk>
2023-08-29 19:20:55 +0530

    Course Profile Page connected to back end

--- src/pages/InstituteStaff/CourseDetail.js
+++ src/pages/InstituteStaff/CourseDetail.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   Text,
   Box,
@@ -10,14 +11,16 @@ import {
   Select,
   Spacer,
   Input,
+  Button,
+  Badge,
 } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
 import data from "./data/data.json";
-import { TimeIcon } from "@chakra-ui/icons";
+import { TimeIcon, CalendarIcon } from "@chakra-ui/icons";
 import { FaMoneyBillAlt } from "react-icons/fa";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
-import StudentsList from "./StudentsList";
+import { useNavigate } from "react-router-dom";
 import {
   Table,
   Thead,
@@ -30,22 +33,36 @@ import {
   TableContainer,
   Avatar,
 } from "@chakra-ui/react";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 const coursepayments = data.Coursepayments;
 
 function CourseDetail() {
+  const navigate = useNavigate();
+
   const { id } = useParams();
-  const courseIdNum = parseInt(id);
+  const [courseDetails, setCourseDetails] = useState(null);
+  const axiosPrivate = useAxiosPrivate();
+  useEffect(() => {
+    const fetchCourseDetails = async () => {
+      try {
+        const response = await axiosPrivate.get(`/staff/course/${id}`);
+        setCourseDetails(response.data);
+      } catch (error) {
+        console.error("Error fetching course details:", error);
+      }
+    };
 
-  // Find the course in the JSON data
-  const course = data.Tutors.flatMap((tutor) => tutor.courses).find(
-    (course) => course.course_id === courseIdNum
-  );
+    fetchCourseDetails();
+  }, [axiosPrivate, id]);
 
-  // Check if the course data is found
-  if (!course) {
-    return <div>Course not found</div>;
+  if (!courseDetails) {
+    return <div>Loading...</div>;
   }
+
+  const handleViewProfile = (stuId) => {
+    navigate(`/staff/stu-profile/${stuId}`);
+  };
 
   const scrollbarStyles = `
     ::-webkit-scrollbar {
@@ -71,7 +88,7 @@ function CourseDetail() {
   return (
     <Box backgroundColor="#F9F9F9" width="100%">
       <Text fontWeight="bold" fontSize="16px" mb={1}>
-        {course.course_name} - {course.name}
+        {courseDetails.title} - {courseDetails.subject}
       </Text>
       <Grid templateColumns="repeat(8, 1fr)" gap={3} marginBottom={5}>
         <GridItem
@@ -98,58 +115,146 @@ function CourseDetail() {
             </TabList>
             <TabPanels>
               <TabPanel>
-                <StudentsList></StudentsList>
-              </TabPanel>
-              <TabPanel>
-                <Box height="550px" overflowY="scroll" css={scrollbarStyles}>
-                  <TableContainer>
-                    <Table variant="simple">
-                      <TableCaption>Payment Details</TableCaption>
-                      <Thead>
-                        <Tr fontSize={13}>
-                          <Th>Date</Th>
-                          <Th>Profile</Th>
-                          <Th>Payment Method</Th>
-                          <Th>Study Pack ID</Th>
+                <Box width="100%" height="79.2vh">
+                  <SimpleGrid
+                    columns={[1, 2, 3, 4]}
+                    spacing="6"
+                    marginLeft={4}
+                    marginRight={4}
+                  >
+                    {courseDetails.student_enrolled_course.map((enrollment) => (
+                      <Box
+                        // key={stu.id}
+                        borderWidth="1px"
+                        borderRadius="lg"
+                        p="4"
+                        shadow="md"
+                        bg="white"
+                        mb="1"
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        key={enrollment.student.id}
+                      >
+                        <Avatar
+                          // src={stu.profileImage}
+                          src={enrollment.student.profile_picture}
+                          mb="2"
+                          size="xl"
+                        />
+                        <Text fontWeight="bold" fontSize={13}>
+                          {/* {stu.fName} {stu.lName} */}
+                          {enrollment.student.first_name}{" "}
+                          {enrollment.student.last_name}
+                        </Text>
+                        <Text fontSize={13}>
+                          {/* {stu.gender} */}
+                          {enrollment.student.gender}
+                        </Text>
+                        {/* {courseDetails.student_enrolled_course.map((enrolledStudent) => (
+  <Box key={enrolledStudent.student.id}>
+    <Badge
+      colorScheme={
+        enrolledStudent.student.students[0].stream === "Mathematics"
+          ? "red"
+          : enrolledStudent.student.students[0].stream === "Biology"
+          ? "green"
+          : enrolledStudent.student.students[0].stream === "Arts"
+          ? "teal"
+          : "orange"
+      }
+    >
+      <Text fontSize={13}>
+        {enrolledStudent.student.students[0].stream}
+      </Text>
+    </Badge>
+  </Box>
+))} */}
 
-                          {/* <Th >Action</Th> */}
-                        </Tr>
-                      </Thead>
-                      {coursepayments.map((payment) => (
-                      <Tbody>
-                        <Tr  key={payment.payment_id} >
-                          <Td fontSize={13}>{payment.purchased_date}</Td>
-                          <Td>
-                            <Flex gap={4}>
-                              <Avatar src={payment.ProfileImage} />
-                              <Text fontSize={13} marginTop={4}>{payment.name}</Text>
-                            </Flex>
-                          </Td>
-                          <Td paddingLeft={-14}>
-                            <Text
-                              marginLeft={4}
-                              textAlign="center"
-                              width="120px"
-                              fontSize={13}
-                              color="white"
-                              borderRadius={15}
-                              px={2}
-                              py={1}
-                              bg={payment.payment_type === "Physical" ? "green.500" : "blue.500"}
-                            >
-                              {payment.payment_type}
-                            </Text>
-                          </Td>
-                          <Td fontSize={13}>{payment.studypack_id}</Td>
-                          {/* <Td><Icon as={EditIcon} /></Td> */}
-                        </Tr>
-                       
-                      </Tbody>
-                       ))}
-                    </Table>
-                  </TableContainer>
+                        <Text fontSize={13}>
+                          Joined Date:
+                          {/* {stu.joinedDate} */}
+                          {new Date(
+                            enrollment.student.join_date
+                          ).toLocaleDateString()}
+                        </Text>
+                        <Text fontSize={13}>
+                          Enrolled Date:
+                          {/* {stu.joinedDate} */}
+                          {new Date(
+                            enrollment.enrolled_at
+                          ).toLocaleDateString()}
+                        </Text>
+                        <Button
+                          size="sm"
+                          colorScheme="blue"
+                          marginTop={2}
+                          onClick={() =>
+                            handleViewProfile(enrollment.student.id)
+                          }
+                        >
+                          View Profile
+                        </Button>
+                      </Box>
+                    ))}
+                  </SimpleGrid>
                 </Box>
               </TabPanel>
+              <TabPanel>
+  <Box height="550px" overflowY="scroll" css={scrollbarStyles}>
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>Payment Details</TableCaption>
+        <Thead>
+          <Tr fontSize={13}>
+            <Th>Date</Th>
+            <Th>Student</Th>
+            <Th>Payment Method</Th>
+            <Th>Study Pack ID</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {courseDetails.study_pack.map((pack) =>
+            pack.student_purchase_studypack.map((payment) => (
+              <Tr key={payment.id}>
+                <Td fontSize={13}>
+                  {new Date(payment.purchased_at).toLocaleDateString()}
+                </Td>
+                <Td>
+                  <Flex gap={4}>
+                    <Avatar src={payment.student.profile_picture} />
+                    <Text fontSize={13} marginTop={4}>
+                      {payment.student.first_name} {payment.student.last_name}
+                    </Text>
+                  </Flex>
+                </Td>
+                <Td paddingLeft={-14}>
+                  <Text
+                    marginLeft={4}
+                    textAlign="center"
+                    width="120px"
+                    fontSize={13}
+                    color="white"
+                    borderRadius={15}
+                    px={2}
+                    py={1}
+                    bg={
+                      payment.type === "PHYSICAL" ? "green.500" : "blue.500"
+                    }
+                  >
+                    {payment.type}
+                  </Text>
+                </Td>
+                <Td fontSize={13}>{pack.id}</Td>
+              </Tr>
+            ))
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  </Box>
+</TabPanel>
+
             </TabPanels>
           </Tabs>
         </GridItem>
@@ -166,34 +271,40 @@ function CourseDetail() {
             border="0.05px solid #DAE6F0"
             height="87vh"
             mr={4}
+            overflowY="scroll"
+            css={scrollbarStyles}
           >
             <Box>
               <Text fontWeight="bold" fontSize="16px" mt={3} mb={4}>
-                {course.course_name} - {course.name}
+                {courseDetails.title} - {courseDetails.subject}
               </Text>
             </Box>
             <SimpleGrid columns={1}>
               <Box>
-                <Image src={course.CourseImage} mb={4}></Image>
+                <Image src={courseDetails.thumbnail} mb={4}></Image>
                 <Box mb={4}>
-                  <Flex gap={10}>
-                    <Flex>
+                  <Flex justify="space-between">
+                    <Flex ml={2}>
                       <Box>
-                        <Icon as={TimeIcon}></Icon>
+                        <Icon as={CalendarIcon}></Icon>
                       </Box>
                       <Box>
                         <Text fontSize="xs" pt={1.5} pl={2}>
-                          20 h 20 mins
+                          {courseDetails.start_date
+                            ? new Date(
+                                courseDetails.start_date
+                              ).toLocaleDateString()
+                            : ""}
                         </Text>
                       </Box>
                     </Flex>
-                    <Flex>
+                    <Flex mr={2}>
                       <Box>
                         <Icon as={FaMoneyBillAlt} width={6} mt={1.5}></Icon>
                       </Box>
                       <Box>
                         <Text fontSize="xs" pt={1.5} pl={2}>
-                          {course.price}
+                          {courseDetails.monthly_fee}
                         </Text>
                       </Box>
                     </Flex>
@@ -202,7 +313,7 @@ function CourseDetail() {
                 <Text fontSize="small" fontWeight="bold" mt={4} mb={4}>
                   Description
                 </Text>
-                <Text fontSize="small">{course.course_description}</Text>
+                <Text fontSize="small">{courseDetails.description}</Text>
               </Box>
             </SimpleGrid>
           </Box>
