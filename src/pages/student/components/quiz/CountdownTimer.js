5bf8ffc3b9103b08f207210f5e5273b83b174dec
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-09-13 16:22:18 +0530

    Modify for exit from quiz when timeout

--- src/pages/student/components/quiz/CountdownTimer.js
+++ src/pages/student/components/quiz/CountdownTimer.js
@@ -1,12 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, useToast } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-function CountdownTimer({time}) {
+import useAxiosPrivate from "../../../../hooks/useAxiosPrivate";
+import { resetQuiz } from "../../../../hooks/reduxReducers/fetchQuestions";
+const STUDENT_QUIZ_DONE_URL = "/stu/quiz";
+
+function CountdownTimer({ time }) {
+  const toast = useToast();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const axiosPrivate = useAxiosPrivate();
+
   const initialTimeInSeconds = time; // 2 hours in seconds
   const [timeRemaining, setTimeRemaining] = useState(
     // Use local storage value if available, or use the initial value
-    () => parseInt(localStorage.getItem("timeRemaining")) || initialTimeInSeconds
+    () =>
+      parseInt(localStorage.getItem("timeRemaining")) || initialTimeInSeconds
   );
+
+  const { mcqName, subject } = useSelector((state) => state.questions);
+  const { result } = useSelector((state) => state.result);
+
+  const handleSubmit = async () => {
+    let currentTime = new Date();
+    currentTime = currentTime.toISOString();
+
+    try {
+      const response = await axiosPrivate.patch(STUDENT_QUIZ_DONE_URL, {
+        subject,
+        quizName: mcqName,
+        result,
+        startTime: localStorage.getItem("startTime"),
+        endTime: currentTime,
+      });
+
+      console.log(localStorage.getItem("startTime"));
+      localStorage.removeItem("startTime");
+      dispatch(resetQuiz());
+
+      toast({
+        title: "Quiz is timed out",
+        description: "The time has passed. Try to finish early on next time",
+        status: "warning",
+        duration: 5000,
+        position: "top-right",
+        isClosable: true,
+      });
+      navigate(`/stu/quizzes/${subject}/${mcqName}/review`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     // Timer function to update time remaining every second
@@ -23,6 +69,10 @@ function CountdownTimer({time}) {
   useEffect(() => {
     // Save the remaining time to local storage whenever it changes
     localStorage.setItem("timeRemaining", timeRemaining.toString());
+
+    if (timeRemaining < 1) {
+      handleSubmit();
+    }
   }, [timeRemaining]);
 
   const formatTime = (timeInSeconds) => {
@@ -40,7 +90,7 @@ function CountdownTimer({time}) {
       direction="column"
       alignItems="center"
       justifyContent="center"
-      bg={timeRemaining < 30 ? '#FDE6E6' : '#D3F3D2'}
+      bg={timeRemaining < 30 ? "#FDE6E6" : "#D3F3D2"}
       ml="10%"
       mr="10%"
       h={14}
@@ -50,7 +100,7 @@ function CountdownTimer({time}) {
         fontSize="3xl"
         fontWeight="semibold"
         textAlign="center"
-        color={timeRemaining < 30 ? '#EF7373' : '#15BD66'}
+        color={timeRemaining < 30 ? "#EF7373" : "#15BD66"}
       >
         {formatTime(timeRemaining)}
       </Box>
