366590596f503e1362baecf0f6f557dedbd60b24
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-09-27 08:39:02 +0530

    Make changes on parent settings

--- src/pages/parent/components/SettingsLayout.js
+++ src/pages/parent/components/SettingsLayout.js
@@ -10,22 +10,13 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 import ProfileSettings from "./ProfileSettings";
 import SecurityInfo from "../../student/components/settings/SecurityInfo";
 import Profile from "../../../assests/images/profile.jpg";
-import useLogout from "../../../hooks/useLogout";
 
 function SettingsLayout() {
-  const logout = useLogout();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    console.log("Logout");
-    await logout();
-    navigate("/login");
-  };
+  
   return (
     <Box w={"calc(100vw - 620px)"} mt={6}>
       <Text
@@ -34,7 +25,6 @@ function SettingsLayout() {
         fontSize={20}
         color={"#444444"}
         ml={10}
-        onClick={handleLogout}
       >
         Parent Profile
       </Text>
